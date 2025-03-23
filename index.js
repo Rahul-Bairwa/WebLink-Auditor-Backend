446fae221bebf6101ab5506a9fb3d9eef28321f6
rahulbairwa12 <rahul.bairwaprpwebs@gmail.com>
2025-03-23 17:35:52 +0530

    Remove specific origins from CORS configuration

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import cors from 'cors';
 const app = express();
 const port = 5200;
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://your-deployed-frontend.com'],
+  origin: '',
   methods: 'GET,POST',
   allowedHeaders: 'Content-Type',
 }));
